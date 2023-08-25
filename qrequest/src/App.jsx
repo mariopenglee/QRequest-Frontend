01f1a65f7ed43d42f5d8a95f04267bb523f32af7
MariusPL <48773928+mariopenglee@users.noreply.github.com>
2023-08-24 19:03:09 -0700

    todo

--- qrequest/src/App.jsx
+++ qrequest/src/App.jsx
@@ -1,65 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-
-const sectionsData = [
-  { id: 1, title: 'Home', content: 'Welcome to our restaurant!' },
-  { id: 2, title: 'Starters', content: 'Check out our delicious starters.' },
-  { id: 3, title: 'Main Courses', content: 'Check out our delicious main courses.' },
-  { id: 4, title: 'Desserts', content: 'Check out our delicious desserts.' },
-  { id: 5, title: 'Drinks', content: 'Check out our delicious drinks.' },
-  // ... Add more sections as needed
-];
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Landing from './Landing';
 
 function App() {
-  const [activeSection, setActiveSection] = useState(1);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-
-      sectionsData.forEach((section, index) => {
-        const sectionElement = document.getElementById(`section-${section.id}`);
-        if (sectionElement) {
-          const sectionTop = sectionElement.offsetTop - 50; // Adjust for navbar height
-          const sectionBottom = sectionTop + sectionElement.clientHeight;
-
-          if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
-            setActiveSection(section.id);
-          }
-        }
-      });
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
-    <div className="app">
-      <nav className="navbar">
-        {sectionsData.map(section => (
-          <a
-            key={section.id}
-            href={`#section-${section.id}`}
-            className={`nav-link ${activeSection === section.id ? 'active' : ''}`}
-          >
-            {section.title}
-          </a>
-        ))}
-      </nav>
-      <div className="scroll-container">
-        {sectionsData.map(section => (
-          <div
-            key={section.id}
-            id={`section-${section.id}`}
-            className={`section ${activeSection === section.id ? 'selected' : ''}`}
-          >
-            <h2>{section.title}</h2>
-            <p>{section.content}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ qrequest/src/Landing.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import './App.css';
+import testicon from './assets/react.svg'
+
+
+const sectionsData = [
+  { id: 1, title: 'Home', content: 'Welcome to our restaurant!', items: [{name: "item1", description: "item1's description"}, {name: "item2", description: "item2's description"}, {name: "item3", description: "item3's description"}]},
+  { id: 2, title: 'Starters', content: 'Check out our delicious starters.', items: [{name: "item1", description: "item1's description"}, {name: "item2", description: "item2's description"}, {name: "item3", description: "item3's description"}]},
+  { id: 3, title: 'Main Courses', content: 'Check out our delicious main courses.', items: [{name: "item1", description: "item1's description"}, {name: "item2", description: "item2's description"}, {name: "item3", description: "item3's description"}]},
+  { id: 4, title: 'Desserts', content: 'Check out our delicious desserts.', items: [{name: "item1", description: "item1's description"}, {name: "item2", description: "item2's description"}, {name: "item3", description: "item3's description"}]},
+  { id: 5, title: 'Drinks', content: 'Check out our delicious drinks.', items: [{name: "item1", description: "item1's description"}, {name: "item2", description: "item2's description"}, {name: "item3", description: "item3's description"}]},
+]
+
+export class Landing extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeSection : 1,
+      restaurantId : 0,
+      restaurantName : "",
+      restaurantImage : "",
+      restaurantSlogan : "",
+    };
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll() {
+    const scrollPosition = window.scrollY;
+
+    sectionsData.forEach((section, index) => {
+      const sectionElement = document.getElementById(`section-${section.id}`);
+      if (sectionElement) {
+        const sectionTop = sectionElement.offsetTop - 50; // Adjust for navbar height
+        const sectionBottom = sectionTop + sectionElement.clientHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+          this.setState({ activeSection : section.id });
+        }
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <nav className="navbar">
+          {sectionsData.map(section => (
+            <a
+              key={section.id}
+              href={`#section-${section.id}`}
+              className={`nav-link ${this.state.activeSection === section.id ? 'active' : ''}`}
+            >
+              {section.title}
+            </a>
+          ))}
+        </nav>
+        <div className="scroll-container">
+          {sectionsData.map(section => (
+            <div
+              key={section.id}
+              id={`section-${section.id}`}
+              className={`section ${this.state.activeSection === section.id ? 'selected' : ''}`}
+            >
+              <h2>{section.title}</h2>
+              <p>{section.content}</p>
+
+              <div className="section-columns">
+                <div className="left">
+                  <img src={testicon} alt="testicon" />
+                </div>
+                <div className="right"> 
+                  {section.items.map(item => (
+                    <div className="item">
+                      <h3>{item.name}</h3>
+                      <p>{item.description}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
